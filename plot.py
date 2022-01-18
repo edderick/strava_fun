f3d72ee152230f0bbf764ddf1a0e75c066d17eea
Edward Seabrook <edderick@live.co.uk>
2022-01-18 00:23:06 -0500

    Glob

--- plot.py
+++ plot.py
@@ -1,32 +1,27 @@
 #!/usr/bin/env python
 
 from xml.etree import ElementTree as ET
-
-import tkinter
-import time
-import threading
-import dateutil.parser
-import math
-from datetime import datetime, timedelta
-
 from PIL import Image, ImageDraw
+from datetime import datetime, timedelta
+import time
+import dateutil.parser
+import glob
 
 WIDTH = 750
 HEIGHT = 750
 
+BORDER = 10
+
 SPEED = 300
 FPS = 12
 
+verbose = False
+
 images = []
 
-trees = [
-    ET.parse("./Just_Keep_Going_Past_the_Library_.gpx"),
-    ET.parse("./Just_Checking_New_York_is_Still_There.gpx"),
-    ET.parse("./Citibike_be_slow_yo.gpx"),
-    ET.parse("./Nowhere_in_Particular.gpx"),
-    ET.parse("./Christmas_Ride.gpx"),
-    ET.parse("./PPx5.gpx"),
-]
+gpx_files = glob.glob("*.gpx")
+
+trees = [ET.parse(gpx_file) for gpx_file in gpx_files]
 
 print(f"Processing {len(trees)} file(s)")
 
@@ -46,7 +41,7 @@ for tree in trees:
     ride = {}
     rides.append(ride)
 
-    ride['data'] = []
+    ride["data"] = []
 
     for trkpt in tree.iter("{http://www.topografix.com/GPX/1/1}trkpt"):
         lat = float(trkpt.get("lat"))
@@ -55,7 +50,7 @@ for tree in trees:
             trkpt.find("{http://www.topografix.com/GPX/1/1}time").text
         )
 
-        ride['data'].append((time, lat, lon))
+        ride["data"].append((time, lat, lon))
 
         min_lat = min(min_lat, lat)
         max_lat = max(max_lat, lat)
@@ -70,97 +65,78 @@ for tree in trees:
 
         num_coors += 1
 
-    ride['start_time'] = start_time
-    ride['end_time'] = end_time
+    ride["start_time"] = start_time
+    ride["end_time"] = end_time
 
     start_time = None
 
-print(f"{min_lat}, {max_lat} -- {min_lon}, {max_lon}")
+print(f"Min Lat: {min_lat}, Max Lat: {max_lat}")
+print(f"Min Lon: {min_lon}, Max Lon: {max_lon}")
 print(f"Number of co-ords: {num_coors}")
-# print(f"Start time: {start_time}, End time: {end_time}")
 
 timer = datetime.now()
-
 time_cursor = timedelta()
 
 run_time = timedelta()
 for ride in rides:
-    run_time = max(ride['end_time'] - ride['start_time'], run_time)
-
+    run_time = max(ride["end_time"] - ride["start_time"], run_time)
 
 count = 0
 
-im = Image.new("RGB", (WIDTH, HEIGHT), "white")
-draw = ImageDraw.Draw(im)
+im = Image.new("RGB", (WIDTH + BORDER * 2, HEIGHT + BORDER * 2), "white")
 pixels = im.load()
-
-last_x = [None for _ in trees]
-last_y = [None for _ in trees]
-
 
 while time_cursor < run_time:
     last_cursor = time_cursor
     time_cursor += timedelta(seconds=(1 / FPS) * SPEED)
     count += 1
-    print(f"Frame: {count} at {time_cursor}")
+
+    if verbose:
+        print(f"Frame: {count} at {time_cursor}")
+
+    leaders = []
 
     for i, ride in enumerate(rides):
-
-        # clearour the leader TODO: Find more elegant way
-        x = last_x[i]
-        y = last_y[i]
-        if x and y:
-            try:
-                pixels[x, y] = (255, 0, 0)
-                pixels[x + 1, y] = (255, 255, 255)
-                pixels[x + 1, y + 1] = (255, 255, 255)
-                pixels[x, y + 1] = (255, 255, 255)
-            except:
-                pass
-
-        for datum in ride['data']:
+        for datum in ride["data"]:
             event_time = datum[0]
             lat = float(datum[1])
             lon = float(datum[2])
 
             # This result in a bug, need to figure out off by one
-            if event_time - rides[i]['start_time'] < last_cursor:
+            if event_time - rides[i]["start_time"] < last_cursor:
                 continue
 
-            x = (lon - min_lon) * (WIDTH / (max_lon - min_lon))
-            y = HEIGHT - ((lat - min_lat) * (HEIGHT / (max_lat - min_lat)))
+            x = (lon - min_lon) * (WIDTH / (max_lon - min_lon)) + BORDER
+            y = HEIGHT - ((lat - min_lat) * (HEIGHT / (max_lat - min_lat))) + BORDER
 
-            last_x[i] = x
-            last_y[i] = y
-
-            # frame.ellipse((x, y, x+1, y+1), fill=red)
             try:
                 pixels[x, y] = (255, 0, 0)
             except:
                 pass  # TODO: Clip out of bounds
 
-            if event_time - ride['start_time'] > time_cursor:
-                try:
-                    pixels[x, y] = (0, 255, 0)
-                    pixels[x + 1, y] = (0, 255, 0)
-                    pixels[x + 1, y + 1] = (0, 255, 0)
-                    pixels[x, y + 1] = (0, 255, 0)
-                except:
-                    pass  # TODO: Clip out of bounds
+            if event_time - ride["start_time"] > time_cursor:
+                leaders.append((x, y))
                 break
 
-    images.append(im.copy())
+    frame = im.copy()
+    for (x, y) in leaders:
+        draw = ImageDraw.Draw(frame)
+        try:
+            draw.ellipse((x - 1, y - 1, x + 1, y + 1), fill="green", outline="green")
+        except Exception as e:
+            print(f"Sorry: {e}")
+            pass  # TODO: Clip out of bounds
 
-print("Done plotting, entering the main loop")
+    images.append(frame)
 
-print(len(images))
+print("Done plotting, saving file...")
 
 # Freeze the last frame
-for _ in range(1, 50):
+for _ in range(0, 50):
     images.append(im.copy())
 
 images[0].save(
-    "./pillow_imagedraw.gif",
+    "./animation.gif",
     save_all=True,
     append_images=images[1:],
     optimize=True,
@@ -168,4 +144,4 @@ images[0].save(
     loop=0,
 )
 
-print(f"Generated in {datetime.now() - timer}")
+print(f"Generated {len(images) - 50} frames in {datetime.now() - timer}")
