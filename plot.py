e6c00d13157eea96deb7886553361851ec65c809
Edward Seabrook <edderick@live.co.uk>
2022-01-17 23:52:18 -0500

    Optimize

--- plot.py
+++ plot.py
@@ -11,8 +11,8 @@ from datetime import datetime, timedelta
 
 from PIL import Image, ImageDraw
 
-WIDTH = 500
-HEIGHT = 500
+WIDTH = 750
+HEIGHT = 750
 
 SPEED = 300
 FPS = 12
@@ -40,32 +40,38 @@ start_time = None
 end_time = None
 num_coors = 0
 
-start_times = []
-end_times = []
+rides = []
 
 for tree in trees:
+    ride = {}
+    rides.append(ride)
+
+    ride['data'] = []
+
     for trkpt in tree.iter("{http://www.topografix.com/GPX/1/1}trkpt"):
         lat = float(trkpt.get("lat"))
         lon = float(trkpt.get("lon"))
+        time = dateutil.parser.isoparse(
+            trkpt.find("{http://www.topografix.com/GPX/1/1}time").text
+        )
+
+        ride['data'].append((time, lat, lon))
+
         min_lat = min(min_lat, lat)
         max_lat = max(max_lat, lat)
         min_lon = min(min_lon, lon)
         max_lon = max(max_lon, lon)
 
         if start_time is None:
-            start_time = dateutil.parser.isoparse(
-                trkpt.find("{http://www.topografix.com/GPX/1/1}time").text
-            )
+            start_time = time
 
         # Who cares about efficiency?
-        end_time = dateutil.parser.isoparse(
-            trkpt.find("{http://www.topografix.com/GPX/1/1}time").text
-        )
+        end_time = time
 
         num_coors += 1
 
-    start_times.append(start_time)
-    end_times.append(end_time)
+    ride['start_time'] = start_time
+    ride['end_time'] = end_time
 
     start_time = None
 
@@ -78,8 +84,8 @@ timer = datetime.now()
 time_cursor = timedelta()
 
 run_time = timedelta()
-for start, end in zip(start_times, end_times):
-    run_time = max(end - start, run_time)
+for ride in rides:
+    run_time = max(ride['end_time'] - ride['start_time'], run_time)
 
 
 count = 0
@@ -98,7 +104,7 @@ while time_cursor < run_time:
     count += 1
     print(f"Frame: {count} at {time_cursor}")
 
-    for i, tree in enumerate(trees):
+    for i, ride in enumerate(rides):
 
         # clearour the leader TODO: Find more elegant way
         x = last_x[i]
@@ -112,18 +118,14 @@ while time_cursor < run_time:
             except:
                 pass
 
-        for trkpt in tree.iter("{http://www.topografix.com/GPX/1/1}trkpt"):
-            event_time = dateutil.parser.isoparse(
-                trkpt.find("{http://www.topografix.com/GPX/1/1}time").text
-            )
+        for datum in ride['data']:
+            event_time = datum[0]
+            lat = float(datum[1])
+            lon = float(datum[2])
 
             # This result in a bug, need to figure out off by one
-            if event_time - start_times[i] < last_cursor:
+            if event_time - rides[i]['start_time'] < last_cursor:
                 continue
-
-            lat = float(trkpt.get("lat"))
-            lon = float(trkpt.get("lon"))
-            # print(lat, lon)
 
             x = (lon - min_lon) * (WIDTH / (max_lon - min_lon))
             y = HEIGHT - ((lat - min_lat) * (HEIGHT / (max_lat - min_lat)))
@@ -137,7 +139,7 @@ while time_cursor < run_time:
             except:
                 pass  # TODO: Clip out of bounds
 
-            if event_time - start_times[i] > time_cursor:
+            if event_time - ride['start_time'] > time_cursor:
                 try:
                     pixels[x, y] = (0, 255, 0)
                     pixels[x + 1, y] = (0, 255, 0)
