2c7674cba5c85f20d7c307743e7bbacdc360690b
Edward Seabrook <edderick@live.co.uk>
2022-01-22 10:16:33 -0500

    Refactor

--- fit_reader.py
+++ fit_reader.py
@@ -1,9 +1,13 @@
 import struct
 
-VERBOSE=False
+VERBOSE = False
 
-PRINT_DATA=True
-PRINT_DEFINITIONS=False
+PRINT_DATA = True
+PRINT_DEFINITIONS = False
+
+FILE_NAME = "./_Mild_.fit"
+# FILE_NAME='./Activity.fit'
+
 
 def get_bit(value, bit_index):
     return (value & (1 << bit_index)) >> bit_index
@@ -13,7 +17,7 @@ data_definitions = {}
 
 global_message_names = {
     0: "FILE_ID",
-    2: "DEVICE_SETTINGS", 
+    2: "DEVICE_SETTINGS",
     3: "USER_PROFILE",
     7: "ZONES_TARGET",
     12: "SPORT",
@@ -22,11 +26,11 @@ global_message_names = {
     18: "SESSION",
     19: "LAP",
     20: "RECORD",
-    21: "EVENT", 
+    21: "EVENT",
     22: "UNKNOWN",
     23: "DEVICE_INFO",
     34: "ACTIVITY",
-    49: "FILE_CREATOR", 
+    49: "FILE_CREATOR",
     78: "HRV",
     79: "UNKNOWN",
     104: "UNKNOWN",
@@ -42,33 +46,35 @@ def process_file_header(fit_file):
     header_size = fit_file.read(1)[0]
     header = fit_file.read(header_size - 1)
 
-    protocol_version, profile_version, data_size, data_type, crc = struct.unpack('<BHI4sH', header)
+    protocol_version, profile_version, data_size, data_type, crc = struct.unpack(
+        "<BHI4sH", header
+    )
 
-    print(f'=====================================')
-    print(f'Header Size: {header_size}')
-    print(f'Protocol Version: {protocol_version}')
-    print(f'Profile Version: {profile_version}')
-    print(f'Data Size: {data_size}')
-    print(f'Data Type: {data_type}')
-    print(f'CRC: {crc}')
-    print(f'=====================================')
+    print(f"=====================================")
+    print(f"Header Size: {header_size}")
+    print(f"Protocol Version: {protocol_version}")
+    print(f"Profile Version: {profile_version}")
+    print(f"Data Size: {data_size}")
+    print(f"Data Type: {data_type}")
+    print(f"CRC: {crc}")
+    print(f"=====================================")
 
-    return data_size # TODO: Return something structured
+    return data_size  # TODO: Return something structured
 
 
-def process_record(fit_file): 
-    record_header = fit_file.read(1)[0] 
+def process_record(fit_file):
+    record_header = fit_file.read(1)[0]
 
     normal_header = get_bit(record_header, 7)
     message_type = get_bit(record_header, 6)
     message_type_specific = get_bit(record_header, 5)
     local_message_num = record_header & 0xF
-    
+
     if VERBOSE:
         if normal_header == 0:
-            print(f'Header Type: Normal Header')
-        else: 
-            print(f'Header Type: Compressed Timestamp Header')
+            print(f"Header Type: Normal Header")
+        else:
+            print(f"Header Type: Compressed Timestamp Header")
 
     if normal_header == 1:
         # TODO: Support this
@@ -76,124 +82,145 @@ def process_record(fit_file):
 
     if message_type == 0:
         process_data_message(fit_file, local_message_num)
-    else: 
+    else:
         process_definition_message(fit_file, local_message_num, message_type_specific)
         if VERBOSE:
-            print(f'Position: {fit_file.tell()}')
+            print(f"Position: {fit_file.tell()}")
 
 
 def process_data_message(fit_file, local_message_num):
     if VERBOSE:
-        print(f'Message Type: Data Message')
-        print(f'Local Message Num: {local_message_num}')
-    
+        print(f"Message Type: Data Message")
+        print(f"Local Message Num: {local_message_num}")
+
     data_definition = data_definitions[local_message_num]
-    
+
     if PRINT_DATA:
-        print(f'Message Type: {global_message_names[data_definition["global_message_num"]]}')
-    for i, field in enumerate(data_definition['fields']):
-        value = fit_file.read(field['field_size'])
-        try: 
-            unpacked, = struct.unpack(f'{data_definition["endian_modifier"]}{field["unpack_string"]}', value)
+        print(
+            f'Message Type: {global_message_names[data_definition["global_message_num"]]}'
+        )
+    for i, field in enumerate(data_definition["fields"]):
+        value = fit_file.read(field["field_size"])
+        try:
+            (unpacked,) = struct.unpack(
+                f'{data_definition["endian_modifier"]}{field["unpack_string"]}', value
+            )
             if VERBOSE:
                 print(f'field[{i}/{field["field_definition_number"]}] = {unpacked}')
-        except: 
+        except:
             if PRINT_DATA:
-                print(f"Failed to unpack value: {value} as {field['base_type_name']} with {field['unpack_string']}. Size={field['field_size']}")
+                print(
+                    f"Failed to unpack value: {value} as {field['base_type_name']} with {field['unpack_string']}. Size={field['field_size']}"
+                )
 
 
-def process_field(fit_file, local_message_num):
+def process_field_definition(fit_file, local_message_num):
     field = fit_file.read(3)
 
-    field_definition_number, field_size, base_type = struct.unpack('BBB', field)
+    field_definition_number, field_size, base_type = struct.unpack("BBB", field)
 
     base_type_number = 0x0F & base_type
 
-    base_types = [
-        'enum', 'sint8', 'uint8', 'sint16', 'uint16', 'sint32', 'uint32', 
-        'string', 'float32', 'float64', 'uint8z', 'uint16z', 'uint32z', 
-        'byte', 'sint64', 'uint64', 'unit64z'
-    ]
+    data_types = {
+        0: {"name": "enum", "string": "b"},
+        1: {"name": "sint8", "string": "b"},
+        2: {"name": "uint8", "string": "B"},
+        3: {"name": "sint16", "string": "h"},
+        4: {"name": "uint16", "string": "H"},
+        5: {"name": "sint32", "string": "i"},
+        6: {"name": "uint32", "string": "I"},
+        7: {"name": "string", "string": f"{field_size}s"},
+        8: {"name": "float32", "string": "f"},
+        9: {"name": "float64", "string": "d"},
+        10: {"name": "uint8z", "string": "B"},
+        11: {"name": "uint16z", "string": "H"},
+        12: {"name": "uint32z", "string": "I"},
+        13: {"name": "byte", "string": "b"},
+        14: {"name": "sint64", "string": "q"},
+        15: {"name": "uint64", "string": "Q"},
+        16: {"name": "unit64z", "string": "Q"},
+    }
 
-    unpack_strings = [
-        'b', 'b', 'B', 'h', 'H', 'i', 'I', f'{field_size}s', 'f', 'd', 'B', 'H', 'I', 'b', 'q', 'Q', 'Q'
-    ]
-
-    base_type_name = base_types[base_type_number]
+    base_type_name = data_types[base_type_number]["name"]
+    unpack_string = data_types[base_type_number]["string"]
 
     if VERBOSE or PRINT_DEFINITIONS:
-        print(f'Field[-]: ', end='')
-        print(f'Field Definition Number: {field_definition_number}, ', end='')
-        print(f'Field Size: {field_size}, ', end='')
-        print(f'Base Type: {base_type_name} ({base_type_number})', end='')
+        print(f"Field[-]: ", end="")
+        print(f"Field Definition Number: {field_definition_number}, ", end="")
+        print(f"Field Size: {field_size}, ", end="")
+        print(f"Base Type: {base_type_name} ({base_type_number})", end="")
         print()
 
-    data_definitions[local_message_num]['fields'].append({
-        'field_definition_number': field_definition_number,
-        'field_size': field_size, 
-        'base_type': base_type_number, 
-        'base_type_name': base_type_name, 
-        'unpack_string': unpack_strings[base_type_number]
-    })
+    data_definitions[local_message_num]["fields"].append(
+        {
+            "field_definition_number": field_definition_number,
+            "field_size": field_size,
+            "base_type": base_type_number,
+            "base_type_name": base_type_name,
+            "unpack_string": unpack_string,
+        }
+    )
 
 
 def process_definition_message(fit_file, local_message_num, message_type_specific):
     if VERBOSE or PRINT_DEFINITIONS:
         print()
-        print(f'Message Type: Definition Message')
-        print(f'Local Message Num: {local_message_num}')
+        print(f"Message Type: Definition Message")
+        print(f"Local Message Num: {local_message_num}")
         print(f'Developer Data: {"Yes" if message_type_specific == 1 else "No"}')
 
     # This is a definition message
     record = fit_file.read(2)
-    _, arch = struct.unpack('bb', record)
+    _, arch = struct.unpack("bb", record)
 
-    if arch == 0: 
-        endian_modifier = '<'
+    if arch == 0:
+        endian_modifier = "<"
         if VERBOSE:
-            print(f'Endianness: Little')
-    else: 
-        endian_modifier = '>'
+            print(f"Endianness: Little")
+    else:
+        endian_modifier = ">"
         if VERBOSE:
-            print(f'Endianness: Big')
+            print(f"Endianness: Big")
 
     record = fit_file.read(3)
-    global_message_num, num_fields = struct.unpack(f'{endian_modifier}hb', record)
+    global_message_num, num_fields = struct.unpack(f"{endian_modifier}hb", record)
 
     if VERBOSE or PRINT_DEFINITIONS:
-        print(f'Global Message: {global_message_names[global_message_num]} ({global_message_num})')
-        print(f'Number of Fields: {num_fields}')
+        print(
+            f"Global Message: {global_message_names[global_message_num]} ({global_message_num})"
+        )
+        print(f"Number of Fields: {num_fields}")
 
     data_definitions[local_message_num] = {
-        'global_message_num': global_message_num, 
-        'num_fields': num_fields,
-        'endian_modifier': endian_modifier,
-        'fields': []
+        "global_message_num": global_message_num,
+        "num_fields": num_fields,
+        "endian_modifier": endian_modifier,
+        "fields": [],
     }
 
     for i in range(0, num_fields):
-        process_field(fit_file, local_message_num)
+        process_field_definition(fit_file, local_message_num)
 
     # TODO: Untested but present to prevent Dev Fields biting us
-    if message_type_specific == 1: 
+    if message_type_specific == 1:
         dev_fields = fit_file.read(1)[0]
-        print(f'Dev Fields: {dev_fields}')
-        
-        data_definitions[local_message_num]['num_fields'] += dev_fields
-        
+        print(f"Dev Fields: {dev_fields}")
+
+        data_definitions[local_message_num]["num_fields"] += dev_fields
+
         for i in range(0, dev_fields):
-            #print(f'****** Dev Field[{i}] = {field}')
-            process_field(fit_file, local_message_num)
+            # print(f'****** Dev Field[{i}] = {field}')
+            process_field_definition(fit_file, local_message_num)
 
 
-with open('./_Mild_.fit', 'rb') as fit_file:
-#with open('./Activity.fit', 'rb') as fit_file:
-    
+with open(FILE_NAME, "rb") as fit_file:
+
+    print(f"Processing: {FILE_NAME}")
+
     data_size = process_file_header(fit_file)
-    
+
     while fit_file.tell() < data_size:
         if VERBOSE:
             print()
-        
-        process_record(fit_file)
 
+        process_record(fit_file)
