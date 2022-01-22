e5fd1dc94247e7ceb70af4f46b24c8ea0d0dd91e
Edward Seabrook <edderick@live.co.uk>
2022-01-22 11:18:42 -0500

    Support RECORD records

--- fit_reader.py
+++ fit_reader.py
@@ -3,6 +3,8 @@ import struct
 VERBOSE = False
 
 PRINT_DATA = True
+PRINT_DATA_FIELDS = False
+PRINT_RECORD = True  # Print the fields if the data is of RECORD type
 PRINT_DEFINITIONS = False
 
 FILE_NAME = "./_Mild_.fit"
@@ -39,6 +41,30 @@ global_message_names = {
     206: "FIELD_DESCRIPTION",
     207: "DEVELOPER_DATA_ID",
     216: "UNKNOWN",
+}
+
+fit_record_fields = {
+    253: "TIMESTAMP",
+    0: "POSITION_LAT",
+    1: "POSITION_LON",
+    5: "DISTANCE",
+    11: "TIME_FROM_COURSE",
+    19: "TOTAL_CYCLES",
+    29: "ACCUMULATED_POWER",
+    73: "ENHANCED_SPEED",
+    78: "ENHANCED_ALTITUDE",
+    2: "ALTITUDE",
+    6: "SPEED",
+    61: "UNKNOWN",
+    66: "UNKNOWN",
+    7: "POWER",
+    9: "GRADE",
+    3: "HEART_RATE",
+    4: "CADENCE",
+    13: "TEMPERATURE",
+    50: "ZONE",
+    53: "FRACTIONAL_CADENCE",
+    62: "DEVICE_INDEX",
 }
 
 
@@ -101,20 +127,41 @@ def process_data_message(fit_file, local_message_num):
         )
     for i, field in enumerate(data_definition["fields"]):
         value = fit_file.read(field["field_size"])
+
+        if field["is_developer_data"]:
+            continue  # TODO: Support dev fields
+
         try:
-            (unpacked,) = struct.unpack(
-                f'{data_definition["endian_modifier"]}{field["unpack_string"]}', value
-            )
-            if VERBOSE:
+            if field["field_size"] > field["base_size"]:
+                # It's an array
+                array_length = int(field["field_size"] / field["base_size"])
+                unpack_string = f'{data_definition["endian_modifier"]}{array_length}{field["unpack_string"]}'
+            else:
+                unpack_string = (
+                    f'{data_definition["endian_modifier"]}{field["unpack_string"]}'
+                )
+
+            unpacked = struct.unpack(unpack_string, value)
+
+            if (
+                PRINT_RECORD
+                and global_message_names[data_definition["global_message_num"]]
+                == "RECORD"
+            ):
+                print(
+                    f'field[{i}/{fit_record_fields[field["field_definition_number"]]}] = {unpacked}'
+                )
+            elif VERBOSE or PRINT_DATA_FIELDS:
                 print(f'field[{i}/{field["field_definition_number"]}] = {unpacked}')
-        except:
+
+        except Exception as e:
             if PRINT_DATA:
                 print(
-                    f"Failed to unpack value: {value} as {field['base_type_name']} with {field['unpack_string']}. Size={field['field_size']}"
+                    f"Failed to unpack value: {value} as {field['field_definition_number']} : {field['base_type_name']} with '{unpack_string}'. Size={field['field_size']}. Error: {e}"
                 )
 
 
-def process_field_definition(fit_file, local_message_num):
+def process_field_definition(fit_file, local_message_num, is_developer_data=False):
     field = fit_file.read(3)
 
     field_definition_number, field_size, base_type = struct.unpack("BBB", field)
@@ -122,27 +169,29 @@ def process_field_definition(fit_file, local_message_num):
     base_type_number = 0x0F & base_type
 
     data_types = {
-        0: {"name": "enum", "string": "b"},
-        1: {"name": "sint8", "string": "b"},
-        2: {"name": "uint8", "string": "B"},
-        3: {"name": "sint16", "string": "h"},
-        4: {"name": "uint16", "string": "H"},
-        5: {"name": "sint32", "string": "i"},
-        6: {"name": "uint32", "string": "I"},
-        7: {"name": "string", "string": f"{field_size}s"},
-        8: {"name": "float32", "string": "f"},
-        9: {"name": "float64", "string": "d"},
-        10: {"name": "uint8z", "string": "B"},
-        11: {"name": "uint16z", "string": "H"},
-        12: {"name": "uint32z", "string": "I"},
-        13: {"name": "byte", "string": "b"},
-        14: {"name": "sint64", "string": "q"},
-        15: {"name": "uint64", "string": "Q"},
-        16: {"name": "unit64z", "string": "Q"},
+        0: {"name": "enum", "string": "b", "size": 1},
+        1: {"name": "sint8", "string": "b", "size": 1},
+        2: {"name": "uint8", "string": "B", "size": 1},
+        3: {"name": "sint16", "string": "h", "size": 2},
+        4: {"name": "uint16", "string": "H", "size": 2},
+        5: {"name": "sint32", "string": "i", "size": 4},
+        6: {"name": "uint32", "string": "I", "size": 4},
+        7: {"name": "string", "string": "s", "size": 1},
+        8: {"name": "float32", "string": "f", "size": 4},
+        9: {"name": "float64", "string": "d", "size": 8},
+        10: {"name": "uint8z", "string": "B", "size": 1},
+        11: {"name": "uint16z", "string": "H", "size": 2},
+        12: {"name": "uint32z", "string": "I", "size": 4},
+        13: {"name": "byte", "string": "b", "size": 1},
+        14: {"name": "sint64", "string": "q", "size": 8},
+        15: {"name": "uint64", "string": "Q", "size": 8},
+        16: {"name": "unit64z", "string": "Q", "size": 8},
     }
 
-    base_type_name = data_types[base_type_number]["name"]
-    unpack_string = data_types[base_type_number]["string"]
+    base_type = data_types[base_type_number]
+    base_type_name = base_type["name"]
+    unpack_string = base_type["string"]
+    base_size = base_type["size"]
 
     if VERBOSE or PRINT_DEFINITIONS:
         print(f"Field[-]: ", end="")
@@ -158,6 +207,8 @@ def process_field_definition(fit_file, local_message_num):
             "base_type": base_type_number,
             "base_type_name": base_type_name,
             "unpack_string": unpack_string,
+            "base_size": base_size,
+            "is_developer_data": is_developer_data,
         }
     )
 
@@ -210,7 +261,7 @@ def process_definition_message(fit_file, local_message_num, message_type_specifi
 
         for i in range(0, dev_fields):
             # print(f'****** Dev Field[{i}] = {field}')
-            process_field_definition(fit_file, local_message_num)
+            process_field_definition(fit_file, local_message_num, True)
 
 
 with open(FILE_NAME, "rb") as fit_file:
